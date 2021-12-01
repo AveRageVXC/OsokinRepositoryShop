b8ba8487fdb8fcf4662f3c89d4ac5c8991aad91e
AveRageVXC <67059644+AveRageVXC@users.noreply.github.com>
2021-12-02 01:51:02 +0300

    12.2021.1

--- 1.sql
+++ 1.sql
@@ -1,11 +1,11 @@
-CREATE TABLE users("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" TEXT NOT NULL, "surname" TEXT NOT NULL, "login" TEXT UNIQUE NOT NULL, "password" BIGINT NOT NULL, "telephone" TEXT UNIQUE NOT NULL, "date_of_registration" TEXT NOT NULL);
+CREATE TABLE users("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" TEXT NOT NULL, "surname" TEXT NOT NULL, "login" TEXT UNIQUE NOT NULL, "password" BIGINT NOT NULL, "telephone" TEXT UNIQUE NOT NULL, "date_of_registration" TEXT NOT NULL, "birth_date" TEXT NOT NULL);
 CREATE TABLE products_in_cart("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "user_id" INTEGER NOT NULL, "products_id" INTEGER NOT NULL, "quantity" INTEGER NOT NULL, "order_id" INTEGER);
 CREATE TABLE products("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" TEXT NOT NULL, "price" INTEGER NOT NULL, "quantity" INTEGER NOT NULL);
 CREATE TABLE orders("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "user_id" INTEGER NOT NULL, "cost" INTEGER NOT NULL, "provider" TEXT, "status" TEXT NOT NULL);
 
-INSERT INTO users (name, surname, login, password, telephone, date_of_registration) VALUES ("Ваня", "Иванов", "vanyaivanov@mail.ru", 12312314313, "+79174357634", "2019.12.03");
-INSERT INTO users (name, surname, login, password, telephone, date_of_registration) VALUES ("Пётр", "Чортов", "petya123@mail.ru", 542345341, "+79165434565", "2016.10.14");
-INSERT INTO users (name, surname, login, password, telephone, date_of_registration) VALUES ("Аделина", "Борисова", "tebyalublya@mail.ru", 92304234233, "+79169876677", "2011.01.23");
+INSERT INTO users (name, surname, login, password, telephone, date_of_registration, birth_date) VALUES ("Ваня", "Иванов", "vanyaivanov@mail.ru", 12312314313, "+79174357634", "2019.12.03", "1999.12.30");
+INSERT INTO users (name, surname, login, password, telephone, date_of_registration, birth_date) VALUES ("Пётр", "Чортов", "petya123@mail.ru", 542345341, "+79165434565", "2016.10.14", "1998.05.12");
+INSERT INTO users (name, surname, login, password, telephone, date_of_registration, birth_date) VALUES ("Аделина", "Борисова", "tebyalublya@mail.ru", 92304234233, "+79169876677", "2011.01.23", "1976.10.24");
 	
 INSERT INTO products_in_cart (user_id, products_id, quantity, order_id) VALUES (2, 1, 3, NULL);
 INSERT INTO products_in_cart (user_id, products_id, quantity, order_id) VALUES (1, 2, 2, 2);
--- 2.sql
+++ 2.sql
@@ -1,1 +1,9 @@
 UPDATE users SET date_of_registration = substr(date_of_registration, 1, 4) || '-' || substr(date_of_registration, 6, 2) || '-' || substr(date_of_registration, 9, 2);
+
+SELECT login, MAX(date_of_registration) AS 'last_register' FROM users;
+
+SELECT DISTINCT substr(birth_date, 1, 4) FROM users;
+
+SELECT COUNT(*) AS 'total_items' FROM users;
+
+SELECT AVG(2021 - substr(birth_date, 1, 4)) as 'average_age' FROM users WHERE date_of_registration >= date('now', '-2 months');
